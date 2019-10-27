882d15107f5db4714fe722a55d3ce079b128821b
Saud Shaban <awesomeinfinity8@outlook.com>
2019-10-27 22:08:47 +0300

    Add and configured CORS for security

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 let express = require("express");
+var cors = require('cors');
 let timestampRoutes = require("./routes/timestamp");
 
 let app = express();
-
+app.use(cors());
 app.use(express.static("./public"));
 
 app.use("/api/timestamp/", timestampRoutes);
